a318c455662c86b2e815eab8f2c9d3f950732d60
The-Bedt-Frontend-Developer <karavaev.kir@gmail.com>
2024-11-07 17:24:20 +0300

    Ура, бургер меню

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(window.innerWidth)
+const burgerMenuButtonSvg = document.querySelectorAll(".burger-menu__svg")
+const burgerMenuButton = document.querySelector(".burger-menu__button")
+const burgerMenuList = document.querySelector(".burger-menu__list")
+
+burgerMenuButton.addEventListener('click', () => {
+    burgerMenuButtonSvg.forEach((el) => {el.classList.toggle("active")})
+    burgerMenuList.classList.toggle("active")
+})
